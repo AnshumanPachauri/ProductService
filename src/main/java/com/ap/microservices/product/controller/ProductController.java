68dab94d656261b2b80cf22a9cfa1eac6ef12e23
Anshuman <anshumanpachauri26@gmail.com>
2026-05-10 12:00:00 +0530

    Created product record and controller.

--- src/main/java/com/ap/microservices/product/controller/ProductController.java
+++ src/main/java/com/ap/microservices/product/controller/ProductController.java
@@ -1,10 +1,22 @@
 package com.ap.microservices.product.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ap.microservices.product.dto.ProductRequest;
 
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
 
+	@PostMapping()
+	@ResponseStatus(value = HttpStatus.CREATED)
+	public void createProduct(@RequestBody ProductRequest productRequest) {
+		
+	}
+	
 }
