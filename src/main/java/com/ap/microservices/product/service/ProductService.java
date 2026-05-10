4a58147eb514c8478aca36e0fc8467512e3a55ca
Anshuman <anshumanpachauri26@gmail.com>
2026-05-10 12:00:00 +0530

    Created product Service to create product.

---
+++ src/main/java/com/ap/microservices/product/service/ProductService.java
@@ -0,0 +1,30 @@
+package com.ap.microservices.product.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ap.microservices.product.dto.ProductRequest;
+import com.ap.microservices.product.model.Product;
+import com.ap.microservices.product.repository.ProductRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class ProductService {
+
+	@Autowired
+	private ProductRepository productRepository;
+	
+	public void createProduct(ProductRequest productRequest) {
+		
+		Product product = Product.builder()
+				.name(productRequest.name())
+				.description(productRequest.description())
+				.price(productRequest.price())
+				.build();
+		productRepository.save(product);
+		log.info("Product Created Successfully");
+	}
+	
+}
